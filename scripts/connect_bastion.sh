aaf2e9f787c4f6a03559b6f46863d87d39f307f3
kayma <kayma@hlag.com>
2022-03-31 08:56:57 +0200

    Improve connection script

--- scripts/connect_bastion.sh
+++ scripts/connect_bastion.sh
@@ -9,7 +9,7 @@
 # Requirements:
 #   - install the Session Manager Plugin before
 #   - AWS CLI credentials of the user able to connect to the bastion must be
-#     made available before.
+#     made available before (export AWS_ACCESS_KEY_ID, ...)
 #   - "jq" is used to parse JSON
 #
 # Variables:
@@ -19,92 +19,12 @@
 
 export AWS_DEFAULT_REGION="eu-central-1"
 
-# add your service to the "services" array. "forwarding" mentions the local port,
-# IP or DNS name of the remote cloud service and the remote port separated by ':'
-#
-# we separated all remote services by
-# a) products
-# b) environments
-# c) service name
-#
-# feel free to change this
-service_json='{
-   "product-a":{
-      "deve":{
-         "aws_account_id":1234,
-         "services":[
-            {
-               "name":"service-a_10000",
-               "forwarding":"10000:some.service.in.the.cloud.com:3306"
-            },
-            {
-               "name":"service-b_10001",
-               "forwarding":"10001:some.other.service.in.the.cloud.com:5432"
-            }
-         ]
-      },
-      "qa":{
-         "aws_account_id":1235,
-         "services":[
-           {
-              "name":"service-a_10100",
-              "forwarding":"10100:some.service.in.the.cloud.com:3306"
-           },
-           {
-              "name":"service-b_10101",
-              "forwarding":"10001:some.other.service.in.the.cloud.com:5432"
-           }
-         ]
-      },
-      "prod":{
-         "aws_account_id":1236,
-         "services":[
-           {
-              "name":"service-a_10200",
-              "forwarding":"10200:some.service.in.the.cloud.com:3306"
-           },
-           {
-              "name":"service-b_10201",
-              "forwarding":"10201:some.other.service.in.the.cloud.com:5432"
-           }
-         ]
-      }
-   },
-   "product-b":{
-      "deve":{
-         "aws_account_id":2345,
-         "services":[
-            {
-               "name":"service-c_20001",
-               "forwarding":"20001:some.service.c.in.the.cloud.com:5432"
-            }
-         ]
-      },
-      "qa":{
-         "aws_account_id":2346,
-         "services":[
-           {
-              "name":"service-c_20101",
-              "forwarding":"20101:some.service.c.in.the.cloud.com:5432"
-           }
-        ]
-      },
-      "prod":{
-         "aws_account_id":2347,
-         "services":[
-           {
-              "name":"service-c_20201",
-              "forwarding":"20201:some.service.c.in.the.cloud.com:5432"
-           }
-         ]
-      }
-   }
-}'
+CWD=$(dirname $0)
+BASTION_SERVICES_FILE="$CWD/bastion_services.json"
 
-products=$(echo "${service_json}" | jq -r 'keys[]')
 
 PS3='Product to connect to: '
-options=("$products")
+mapfile -t options < <(jq -r 'keys[]' "$BASTION_SERVICES_FILE")
 select opt in "${options[@]}"
 do
   product=$(tr -dc '[:print:]' <<< "$opt")
@@ -112,26 +32,25 @@ do
 done
 echo
 
-environments=$(echo "${service_json}" | jq -r --arg product "${product}" '.[$product] | keys[]')
-
 PS3='Environment to connect to: '
-options=("$environments")
+mapfile -t options < <(jq -r --arg product "${product}" '.[$product] | keys[]' "$BASTION_SERVICES_FILE")
 select opt in "${options[@]}"
 do
   environment=$(tr -dc '[:print:]' <<< "$opt")
-  aws_account_id=$(echo "${service_json}" | jq -r --arg product "${product}" --arg environment "${environment}"  '.[$product] | .[$environment].aws_account_id')
+  aws_account_id=$(jq -r --arg product "${product}" --arg environment "${environment}"  '.[$product] | .[$environment].aws_account_id' "$BASTION_SERVICES_FILE")
+  bastion_host_name=$(jq -r --arg product "${product}" --arg environment "${environment}"  '.[$product] | .[$environment].bastion_host_name' "$BASTION_SERVICES_FILE")
+  connection_role_name=$(jq -r --arg product "${product}" --arg environment "${environment}"  '.[$product] | .[$environment].connection_role_name' "$BASTION_SERVICES_FILE")
   break
 done
 echo
 
-services=$(echo "${service_json}" | jq -r --arg product "${product}" --arg environment "${environment}"  '.[$product] | .[$environment].services[].name')
-
 PS3='Service to connect to: '
-options=("$services")
+mapfile -t options < <(jq -r --arg product "${product}" --arg environment "${environment}"  '.[$product] | .[$environment].services[].name' "$BASTION_SERVICES_FILE")
 select opt in "${options[@]}"
 do
   service=$(tr -dc '[:print:]' <<< "$opt")
-  port_forwarding=$(echo "${service_json}" | jq -r --arg product "${product}" --arg environment "${environment}" --arg service "${service}" '.[$product] | .[$environment].services[] | select(.name==$service).forwarding')
+  port_forwarding=$(jq -r --arg product "${product}" --arg environment "${environment}" --arg service "${service}" '.[$product] | .[$environment].services[] | select(.name==$service).forwarding' "$BASTION_SERVICES_FILE")
+  region=$(jq -r --arg product "${product}" --arg environment "${environment}" --arg service "${service}" '.[$product] | .[$environment].services[] | select(.name==$service).region' "$BASTION_SERVICES_FILE")
   break
 done
 echo
@@ -145,9 +64,9 @@ ssh_public_key=$(cat "${TEMP_DIRECTORY}/bastion_key.pub")
 
 
 # find the bastion in the cloud: instance-id and availability zone
-BASTION_USER_ROLE_ARN="arn:aws:iam::${aws_account_id}:role/find-bastion"
+BASTION_USER_ROLE_ARN="arn:aws:iam::${aws_account_id}:role/$connection_role_name"
 
-temp_credentials=$(aws sts assume-role --role-arn "${BASTION_USER_ROLE_ARN}" --role-session-name find-bastion --output json)
+temp_credentials=$(aws sts assume-role --role-arn "${BASTION_USER_ROLE_ARN}" --role-session-name connect-bastion --output json)
 AWS_ACCESS_KEY_ID=$(echo "${temp_credentials}" | jq -r .Credentials.AccessKeyId)
 export AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY=$(echo "${temp_credentials}" | jq -r .Credentials.SecretAccessKey)
@@ -155,13 +74,13 @@ export AWS_SECRET_ACCESS_KEY
 AWS_SESSION_TOKEN=$(echo "${temp_credentials}" | jq -r .Credentials.SessionToken)
 export AWS_SESSION_TOKEN
 
-# make sure to match your naming schema here to be able to find the bastion host
-BASTION_HOST_NAME="${environment}-bastion-bastion"
-json=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${BASTION_HOST_NAME}")
+# find all running Bastion hosts
+instances=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=${bastion_host_name}" --region ${region} | jq '[.Reservations[].Instances[] | {instanceId: .InstanceId, availabilityZone: .Placement.AvailabilityZone}]')
+total_instances=$(echo "$instances" | jq -r '. | length')
+selected_instance=$(( RANDOM % total_instances ))
 
-instance_id=$(echo "${json}" | jq -r .Reservations[0].Instances[0].InstanceId)
-az=$(echo "${json}" | jq -r .Reservations[0].Instances[0].Placement.AvailabilityZone)
-
+instance_id=$(echo "$instances" | jq -r .[${selected_instance}].instanceId)
+az=$(echo "$instances" | jq -r .[${selected_instance}].availabilityZone)
 
 # send public key: you have to established the connection within the next 60 seconds. Otherwise the key is automatically removed by AWS.
 aws ec2-instance-connect send-ssh-public-key --instance-id "${instance_id}" --availability-zone "${az}" --instance-os-user ec2-user --ssh-public-key "${ssh_public_key}"
@@ -170,4 +89,4 @@ echo "Connection becomes ready in a couple of seconds ..."
 
 
 # connect via SSH and establish the port forwarding
-ssh "ec2-user@${instance_id}" -i "${TEMP_DIRECTORY}/bastion_key" -N -L "${port_forwarding}" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o ProxyCommand="aws ssm start-session --target %h --document AWS-StartSSHSession --parameters portNumber=%p"
+ssh "ec2-user@${instance_id}" -i "${TEMP_DIRECTORY}/bastion_key" -N -L "${port_forwarding}" -o "ServerAliveInterval=180" -o "ServerAliveCountMax=2" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o ProxyCommand="aws ssm start-session --target %h --document AWS-StartSSHSession --parameters portNumber=%p"
