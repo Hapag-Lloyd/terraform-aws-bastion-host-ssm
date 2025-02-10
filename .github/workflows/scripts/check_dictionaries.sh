578449496ce172c15318c6d9600f7eae66ead819
Matthias Kay <matthias.kay@hlag.com>
2025-02-10 10:40:17 +0100

    chore: update workflows to 1.14.0 (#389)
    # Description
    
    What is the overall goal of your PR? Which problem does it solve? Please
    also include relevant motivation and context.
    List any dependencies that are required for this change.
    
    Fixes #(issue number)
    
    # Migrations required
    
    yes: please describe the migration
    no: please delete the whole paragraph
    
    # Verification
    
    Please describe the test cases you used to verify your code. Did you
    check the change in your environment?
    
    # Checklist
    
    - [ ] My code follows the style guidelines of the project
    - [ ] I have performed a self-review of my own code
    - [ ] I have made corresponding changes to the documentation

---
+++ .github/workflows/scripts/check_dictionaries.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -euo pipefail
+
+#
+# Based on the idea outlined in https://github.com/streetsidesoftware/cspell/issues/2536#issuecomment-1126077282
+# but with a few modifications to fit the needs of our project.
+#
+MISSPELLED_WORDS_PATH="misspelled-words.txt"
+
+CSPELL_CONFIGURATION_FILE=".config/cspell.json"
+DICTIONARIES_PATH=".config/dictionaries"
+mapfile -t DICTIONARY_FILES_TO_CHECK < <(ls .config/dictionaries/*.txt)
+
+export LC_ALL='C'
+
+# Make a list of every misspelled word without any custom dictionaries and configuration file
+cp "$CSPELL_CONFIGURATION_FILE" "${CSPELL_CONFIGURATION_FILE}.temp"
+jq 'del(.dictionaryDefinitions)' "${CSPELL_CONFIGURATION_FILE}.temp" | \
+  jq 'del(.dictionaries)' > "$CSPELL_CONFIGURATION_FILE"
+
+# renovate: datasource=npm depName=@cspell/dict-cspell-bundle
+cspell_dict_version="v1.0.29"
+npm i -D @cspell/dict-cspell-bundle@${cspell_dict_version:1}
+
+# renovate: datasource=npm depName=cspell
+cspell_version="v8.17.1"
+npx cspell@${cspell_version:1} . -c "$CSPELL_CONFIGURATION_FILE" --dot --no-progress --no-summary --unique --words-only \
+  --no-exit-code --exclude "$DICTIONARIES_PATH/**" | sort --ignore-case --unique > "$MISSPELLED_WORDS_PATH"
+
+# Check the custom dictionaries
+ONE_OR_MORE_FAILURES=0
+for DICTIONARY_NAME in "${DICTIONARY_FILES_TO_CHECK[@]}"; do
+  echo "Checking for orphaned words in dictionary: $DICTIONARY_NAME"
+
+  # ensure the dictionary is sorted and unique, fails if not
+  sort --ignore-case --unique --check "$DICTIONARY_NAME" > /dev/null
+
+  # Check that each word in the dictionary is actually being used
+  while IFS= read -r line; do
+    # Remove any trailing newline characters
+    line=$(echo "$line" | tr -d '\r\n')
+
+    if ! grep "$line" "$MISSPELLED_WORDS_PATH" --ignore-case --silent ; then
+      echo "The following word in the $DICTIONARY_NAME dictionary is not being used: ($line)"
+      ONE_OR_MORE_FAILURES=1
+    fi
+  done < "$DICTIONARY_NAME"
+done
+
+rm -f "$MISSPELLED_WORDS_PATH"
+
+if [ $ONE_OR_MORE_FAILURES -ne "0" ]; then
+  echo "Dictionary check failed."
+  exit 1
+fi
+
+echo "All dictionaries are valid."
